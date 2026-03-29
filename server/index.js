3385f01510d1a33b4287c6eb21d474b04c70ddac
Prashant <prashantrajput5000@gmail.com>
2026-03-29 18:19:25 +0530

    database added

--- server/index.js
+++ server/index.js
@@ -1,25 +1,43 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// HTML serve karega
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// 👉 apna MongoDB link yaha paste kar
+const uri = "mongodb+srv://admin:prashant@123@cluster0.xjpxqgw.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri);
+
+let db;
+
+// connect database
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db("chatApp");
+    console.log("MongoDB Connected 🔥");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+connectDB();
+
+app.use(express.static("server"));
 
 io.on("connection", (socket) => {
   console.log("User connected");
 
-  socket.on("message", (data) => {
-    io.emit("message", data); // sabko bhej
-  });
+  socket.on("message", async (data) => {
+    // save in DB
+    await db.collection("messages").insertOne(data);
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
+    // send to all users
+    io.emit("message", data);
   });
 });
 
