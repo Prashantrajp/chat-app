54482f94cd2b920d38a1582b012ac5d073760e2e
Prashant <prashantrajput5000@gmail.com>
2026-03-29 16:15:14 +0530

    final commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(express.static(__dirname));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
